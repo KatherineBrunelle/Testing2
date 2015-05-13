03da7fea1bb61e0b643f8c695e6380c1b7109f18
Katherine Brunelle <kab@Katherines-MacBook-Pro.local>
2015-05-13 10:27:54 -0400

    new file to start coding in

---
+++ new.cpp
@@ -0,0 +1,14 @@
+/*********************************************/
+/*                                           */
+/* Katherine Brunelle Testing 2 W/O tutorial */
+/*                                           */
+/*********************************************/
+
+#include <stdio.h>
+
+int main(int argc, char* argv){
+
+  cout << "Look it's C++ again!";
+
+  return 1;
+}
